edf8bb91c72e37aad9ce07b9e9c4660c83afe32c
Camila Rodriguez <camila.rodriguez.aguila@gmail.com>
2022-03-01 18:59:30 -0700

    Taken notes of class and exercises done during it.

--- First Class.py
+++ First Class.py
@@ -1,1 +1,40 @@
+#Introducing Python to Students
 print("Hello World!")
+
+#Changing Text
+print("Name")
+
+#Initializing Variables
+name = "Camila"
+print(name)
+
+#Printing numbers
+print(475)
+
+#Types of Variables
+#int stands for integer and means number without a fractional part, like 475
+#str stands for string and are letters and are written between quotation marks, like "Hello"
+#float means numbers with a fractional part, like 2.5
+
+#Concatenate
+name = "Camila"
+print("My name is: " + name)
+
+#Change variable name but not print line
+name = "Coco"
+print("My name is: " + name)
+
+# Use of multiple variables and variable's writing format
+first_name = "Jose"
+second_name = "Manuel"
+first_last_name = "Coto"
+print(first_name + second_name + first_last_name)
+#Diffeence between capitalization in variables
+name = "Camila"
+Name = "Jose"
+print(name)
+
+#Printing strings and integers and use of str()
+name = "Camila"
+age = 16
+print("My name is " + name + " and I am " + str(age) + " years old." )
