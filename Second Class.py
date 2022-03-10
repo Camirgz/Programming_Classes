a687564d375f2160222a1157a8aed6cd0490342c
Camila <camila.rodriguez.aguila@gmail.com>
2022-03-10 11:27:14 -0700

    Third Class, done exercises and notes

---
+++ Second Class.py
@@ -0,0 +1,49 @@
+#Date: March 2,2022
+
+#Last class review
+#First Part
+print("Hello World!")
+
+#Second Part
+print("Camila")
+
+#Third Part
+name = "Camila"
+print(name)
+
+#Fourth Part
+name = "José"
+second_name = "Manuel"
+last_name = "Coto"
+
+print(name + second_name + last_name)
+
+#Fifth Part
+name = "José"
+second_name = "Manuel"
+last_name = "Coto"
+
+print("My name is " + name + " " + second_name + " " + last_name)
+
+#Sixth Part
+name = "José"
+second_name = "Manuel"
+last_name = "Coto"
+age = 29
+print("My name is " + name + " " + second_name + " " + last_name + " and I'm " + str(age) + " years old")
+
+#String Methods
+#First Part
+name = "camila"
+place = "cOstA riCa"
+
+print("My name is " + name.capitalize() + " and I live in " + place.title())
+#.capitalize() makes the first letter of the first word upper case
+#.title() makes the first letters of all the words upper case
+
+#Second Part
+cloth = "shirts"
+color = "RED"
+print("I love " + color.lower() + " " + cloth.upper() + " not dresses")
+#.lower() makes all the letters lowercase
+#.upper() makes all the letters upper case
