8433a5417853b2dcdb82507a7b47a3e7d7dcd2db
Camila <camila.rodriguez.aguila@gmail.com>
2022-03-17 06:42:00 -0700

    Fourth Class

---
+++ Fourth Class.py
@@ -0,0 +1,41 @@
+#Date: March 16,2022
+
+#Math Operators
+# + is used for a sum
+# - is used for a subtraction
+# / is used for a division
+# * si used for a multiplication
+# ** is used for a power
+
+#Operations with Variables
+x = 5
+y = 10
+print(x*y)
+
+#Operations on the print function
+x = 4
+print(x/2)
+
+#Operations on the variables
+x = 10+6
+print(x)
+
+#String concatenation and Math Operators Usage
+x = 9
+y = 3
+print(str(x) + " plus " + str(y)+ " equals " + str(x+y))
+
+#Operations with strings (sum)
+x = "He"
+y = "llo"
+print(x+y)
+
+#Operations with strings (multiplication)
+x = "Santa says "
+y = "Ho"
+print(x+y*10)
+
+#Operations with stringed numbers
+x = "12"
+y = "34"
+print(x+y)
