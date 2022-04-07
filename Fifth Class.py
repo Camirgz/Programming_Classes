eb4a6dfa6f797d94d9ee92d72a31599c20a9ec25
Camila <camila.rodriguez.aguila@gmail.com>
2022-04-07 10:45:42 -0700

    Fifth Class

--- Fifth Class.py
+++ Fifth Class.py
@@ -1,2 +1,37 @@
-#Date: March 23,2022
+#Date: April 6,2022
 #input function
+
+#First Approach
+name = input("Enter your name: ")
+print(name)
+
+name = input("Enter your name:")
+print("Your name is " + name)
+
+name = input("Enter your name:")
+print("Your name is " + name.title())
+
+name = input("Enter your name:")
+print("Your name is " + name)
+print("Your name is " + name.title())
+print("Your name is " + name.upper())
+print("Your name is " + name.lower())
+
+#Conversation
+name = input("What's your name? ")
+print("Nice to meet you, " + name + " my name is Camila")
+tv_show = input("What's your favorite TV show? ")
+print("Woooow my favorite TV Show is " + tv_show + " too!")
+age = input("And how old are you? ")
+print("I'm 17! and I love coding!")
+hobby = input("What do you do on your free time?")
+print("Fascinating " + name + ", I also like " + hobby + " but not as much... ")
+
+#Math Operators
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+
+print(str(x) + " + " + str(y) + " = " + str(x+y))
+print(str(x) + " - " + str(y) + " = " + str(x-y))
+print(str(x) + " / " + str(y) + " = " + str(x/y))
+print(str(x) + " * " + str(y) + " = " + str(x*y))
