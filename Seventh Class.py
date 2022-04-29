c4ae37bd99c9983da39622c11c5b3a405ef59157
Camila <camila.rodriguez.aguila@gmail.com>
2022-04-28 17:27:40 -0700

    Seventhth Class

--- Seventh Class.py
+++ Seventh Class.py
@@ -1,1 +1,47 @@
-#
+#April 27,2022
+#if comparatives
+
+#First Exercise
+x = "Hello"
+y = "Good Bye"
+
+if x == y:
+    print("They're equal")
+else:
+    print("They're different")
+
+#Second Exercise
+x = 2
+y = 14
+
+if x == y:
+    print("They're equal")
+else:
+    print("They're different")
+
+#Third Exercise
+x = 475
+y = 475
+
+if x == y:
+    print("They're equal")
+else:
+    print("They're different")
+
+#Fourth Exercise
+x = 20
+y = 15
+
+if x > y:
+    print(str(x) + " is bigger than " + str(y))
+elif x < y:
+    print(str(x) + " is smaller than " + str(y))
+else:
+    print(str(x) + " equals " + str(y))
+
+#Notes
+"""
+Formula for comparatives cycles:
+function + condition + :
+Indentation levels are important
+"""
